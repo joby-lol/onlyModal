547c2fae7f88663e602050c36bd99f43ada1ffd7
Joby Elliott <code@byjoby.com>
2013-10-22 07:11:50 -0600

    Stubbed

---
+++ peBox.js
@@ -0,0 +1,70 @@
+/** @license
+* peBox - MIT License
+* Copyright (c) 2013 Joby Elliott
+* http://go.byjoby.net/peBox
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*/
+
+function peBox(obj,options) {
+	var defaults = {
+		minSize:300
+	};
+	//loop through all options and merge them with defaults
+	//don't do anything with tests or requirements just yet
+	if (typeof(options) == 'object') {
+		for (var prop in options) {
+			if (options.hasOwnProperty(prop)) {
+				switch (prop)
+				{
+					default:
+						defaults[prop] = options[prop];
+				}
+			}
+		}
+	}
+	this.options = defaults;
+	this.obj = obj;
+}
+
+peBox.prototype.checkSize_now = function () {
+	var width = window.innerWidth;
+}
+peBox.prototype.on = function () {
+	
+}
+peBox.prototype.off = function () {
+
+}
+
+/*
+	Note to self, don't debounce in prototype methods, objects need their
+	own debounced methods set up during construction
+*/
+peBox.prototype.debounce = function (func, threshold, execAsap) {
+	var timeout;
+	return function debounced () {
+		var obj = this;
+		var args = arguments;
+		function delayed () {
+			if (!execAsap) {
+				func.apply(obj,args);
+			}
+			timeout = null;
+		}
+		if (timeout) {
+			clearTimeout(timeout);
+		}else if (execAsap) {
+			func.apply(obj,args);
+		}
+		timeout = setTimeout(delayed,threshold||100);
+	};
+}
